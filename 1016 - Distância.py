6141298b0f8152b2f12f286fdecfa97d5ee753d4
Gabriel Souto <soutog@pge.rj.gov.br>
2022-11-22 22:53:30 -0300

    Conteúdo correto para o exercício 1016

--- 1016 - Distância.py
+++ 1016 - Distância.py
@@ -1,5 +1,2 @@
-import math
-x1, y1 = map(float, input().split())
-x2, y2 = map(float, input().split())
-distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-print(f"{distancia:.4f}")
+distancia = int(input())
+print(f"{distancia*2} minutos")
